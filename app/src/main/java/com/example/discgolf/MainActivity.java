2069ee58cbc22b9e5e44fba124cfc2524b9dada8
Jepunen <62857023+Jepunen@users.noreply.github.com>
2022-05-12 10:54:37 +0300

    hhh

---
+++ app/src/main/java/com/example/discgolf/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.discgolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(view -> {
+
+            goToFragment(new NewRound());
+
+        });
+    }
+
+    public void goToFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+}
